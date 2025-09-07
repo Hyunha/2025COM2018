a2286b79f3682e7f07c25a88f36e1b7f8458de6e
Hyunha Kim <lagrato@gmail.com>
2025-09-07 20:56:26 +0900

    add codes : ch#01

---
+++ COM2018/src/chapters/ch01/HelloWorldGUI.java
@@ -0,0 +1,9 @@
+package chapters.ch01;
+
+import javax.swing.*;
+
+public class HelloWorldGUI {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello, World!");
+    }
+}
