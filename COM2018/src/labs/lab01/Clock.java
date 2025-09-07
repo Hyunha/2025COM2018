305d57ba3ad9d1733a28e524ade68cd320b759b8
Hyunha Kim <lagrato@gmail.com>
2025-09-07 20:53:23 +0900

    add codes : lab#01

---
+++ COM2018/src/labs/lab01/Clock.java
@@ -0,0 +1,9 @@
+package labs.lab01;
+
+import java.time.LocalTime;
+
+public class Clock {
+    public static void main(String[] args) {
+        System.out.println(LocalTime.now());
+    }
+}
---
+++ COM2018/src/labs/lab01/ClockGUI.java
@@ -0,0 +1,10 @@
+package labs.lab01;
+
+import javax.swing.*;
+import java.time.LocalTime;
+
+public class ClockGUI {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, LocalTime.now());
+    }
+}
